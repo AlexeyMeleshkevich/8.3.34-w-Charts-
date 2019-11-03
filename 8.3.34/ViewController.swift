fa9e6fc7fc8ee05ce9dc791c27c87952910494fa
Alexey <meleshhhkevich@icloud.com>
2019-11-03 19:39:25 +0300

    Initial Commit

---
+++ 8.3.34/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  8.3.34
+//
+//  Created by Alexey Meleshkevich on 03/11/2019.
+//  Copyright © 2019 Alexey Meleshkevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
